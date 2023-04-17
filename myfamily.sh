ae2d7487d7da66529815d4e8ecb0e2d965556af8
Maxime <maxou.dub76@gmail.com>
2023-04-17 22:35:36 +0200

    v1

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
 export HERO_ID=1
-curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select( .id == 1) .connections .relatives' | tr -d '"'
+curl -s https://zone01normandie.org/assets/superhero/all.json | jq ' .[] | select( .id == 70) .connections .relatives' | tr -d '"'
