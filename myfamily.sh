72f5de0ff9c6dcbe5729d6b38d00975d187b2891
Maxime <maxou.dub76@gmail.com>
2023-04-18 09:48:46 +0200

    v1

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-curl -s "https://zone01normandie.org/assets/superhero/all.json" | jq -r --arg HERO_ID "$HERO_ID" '.[] | select(.id == ($HERO_ID|tonumber)) | .connections .relatives'
+curl -s "https://zone01normandie.org/assets/superhero/all.json" | jq --arg HERO_ID "$HERO_ID" '.[] | select(.id == ($HERO_ID|tonumber)) | .connections.relatives' | sed 's/"//g'
