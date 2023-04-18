3fb8e573e981f9f20cb58cb802b9686b8eebea53
Maxime <maxou.dub76@gmail.com>
2023-04-18 14:12:28 +0200

    v1

--- my_answer.sh
+++ my_answer.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-git clone $(curl -s https://api.github.com/repos/01-edu/the-final-cl-test/git/refs/heads/main | grep '"sha"' | head -n 1 | awk '{print $2}' | tail -c 41) https://github.com/tensorflow/tensorflow.git
+curl -s https://github.com/01-edu/the-final-cl-test | jq ' .help.crimescene'
