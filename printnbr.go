699ef80da8e541809ba184743c03736b466fbb6b
Maxime <maxou.dub76@gmail.com>
2023-04-19 16:52:04 +0200

    1

--- printnbr.go
+++ printnbr.go
@@ -2,13 +2,19 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func printNbr(n int) {
+func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		PrintNbr(-9223372036854775807 / 10)
+		z01.PrintRune('8')
+		return
+	}
+
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
 	}
 	if n >= 10 {
-		printNbr(n / 10)
+		PrintNbr(n / 10)
 	}
 	z01.PrintRune(rune(n%10 + '0'))
 }
