1cef05e9f203df78aff414454270eba68737a821
Maxime <maxou.dub76@gmail.com>
2023-04-19 11:36:23 +0200

    v1

--- printcomb2.go
+++ printcomb2.go
@@ -3,13 +3,18 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := 0; i <= 8; i++ {
-		for j := i + 1; j <= 9; j++ {
-			z01.PrintRune(rune(i + '0'))
-			z01.PrintRune(rune(j + '0'))
-			if i != 8 || j != 9 {
-				z01.PrintRune(',')
+	for i := 0; i <= 98; i++ {
+		for j := i + 1; j <= 99; j++ {
+			if (i/10 != i%10) && (j/10 != j%10) && (i/10 != j/10) && (i%10 != j%10) {
+				z01.PrintRune(rune(i/10 + '0'))
+				z01.PrintRune(rune(i%10 + '0'))
 				z01.PrintRune(' ')
+				z01.PrintRune(rune(j/10 + '0'))
+				z01.PrintRune(rune(j%10 + '0'))
+				if i != 98 || j != 99 {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
 			}
 		}
 	}
