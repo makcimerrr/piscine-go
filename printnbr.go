c48997d746feedda31bbda6376273d98dd4d335a
Maxime <maxou.dub76@gmail.com>
2023-04-19 15:35:41 +0200

    v1

--- printnbr.go
+++ printnbr.go
@@ -7,10 +7,8 @@ func printNbr(n int) {
 		z01.PrintRune('-')
 		n = -n
 	}
-	for n >= 10 {
-		digit := n % 10
-		n /= 10
-		z01.PrintRune(rune(digit + '0'))
+	if n >= 10 {
+		printNbr(n / 10)
 	}
-	z01.PrintRune(rune(n + '0'))
+	z01.PrintRune(rune(n%10 + '0'))
 }
