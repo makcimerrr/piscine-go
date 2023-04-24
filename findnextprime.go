593cf52af8d44502b8a4de9ba09a5602577d9ef7
Maxime <maxou.dub76@gmail.com>
2023-04-24 11:43:30 +0200

    1

---
+++ findnextprime.go
@@ -0,0 +1,41 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb < 2 {
+		return 2
+	}
+
+	i := nb
+	if i%2 == 0 {
+		i++
+	}
+
+	for {
+		if IsPrime(i) {
+			return i
+		}
+		i += 2
+	}
+}
+
+func IsPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+
+	if n == 2 {
+		return true
+	}
+
+	if n%2 == 0 {
+		return false
+	}
+
+	for i := 3; i*i <= n; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
