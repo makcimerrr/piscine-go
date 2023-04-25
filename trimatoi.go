183dbdc323eebcb3a62642a47f579ecfa42f1ed4
Maxime <maxou.dub76@gmail.com>
2023-04-25 15:18:50 +0200

    1

---
+++ trimatoi.go
@@ -0,0 +1,25 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	result := 0
+	sign := 1
+	started := false
+	for _, c := range s {
+		if !started {
+			if c == '-' {
+				sign = -1
+				started = true
+				continue
+			} else if c == '+' {
+				started = true
+				continue
+			}
+		}
+		if c >= '0' && c <= '9' {
+			digit := int(c - '0')
+			result = result*10 + digit
+			started = true
+		}
+	}
+	return result * sign
+}
