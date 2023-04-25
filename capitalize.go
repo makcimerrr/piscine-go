29055651bf17c3d5b8cd64c98a09b2d33c9f735f
Maxime <maxou.dub76@gmail.com>
2023-04-25 15:38:14 +0200

    1

---
+++ capitalize.go
@@ -0,0 +1,11 @@
+package piscine
+
+func Capitalize(s string) string {
+	c := []rune(s)
+	for i := 0; i < len(c); i++ {
+		if (c[i] >= 'a' && c[i] <= 'z') && (i == 0 || c[i-1] == ' ' || c[i-1] == '+') {
+			c[i] = c[i] - 32
+		}
+	}
+	return string(c)
+}
