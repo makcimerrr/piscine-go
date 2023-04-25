06befd570968f8166dbe776aab3882512755b624
Maxime <maxou.dub76@gmail.com>
2023-04-25 16:53:25 +0200

    1

--- capitalize.go
+++ capitalize.go
@@ -1,17 +1,13 @@
 package piscine
 
-import (
-	"unicode"
-)
-
 func Capitalize(s string) string {
-	var res string
-	for i := 0; i < len(s); i++ {
-		if i == 0 || !unicode.IsLetter(rune(s[i-1])) {
-			res += string(unicode.ToUpper(rune(s[i])))
-		} else {
-			res += string(unicode.ToLower(rune(s[i])))
+	c := []rune(s)
+	for i := 0; i < len(c); i++ {
+		if (c[i] >= 'a' && c[i] <= 'z') && (i == 0 || c[i-1] == ' ' || c[i-1] == '+') {
+			c[i] = c[i] - 32
+		} else if c[i] >= 'A' && c[i] <= 'Z' {
+			c[i] = c[i] + 32
 		}
 	}
-	return res
+	return string(c)
 }
