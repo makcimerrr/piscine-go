1fd74bdd03b92e335f1fe854627170077d353982
Maxime <maxou.dub76@gmail.com>
2023-04-25 12:37:54 +0200

    1

--- isupper.go
+++ isupper.go
@@ -1,16 +1,10 @@
 package piscine
 
 func IsUpper(s string) bool {
-	flag := 0
 	for i := 0; i < len([]rune(s)); i++ {
-		if rune(s[i]) >= 'A' && rune(s[i]) <= 'Z' {
-			flag = 1
-		} else {
-			flag = 0
+		if rune(s[i]) < 'A' || rune(s[i]) > 'Z' {
+			return false
 		}
 	}
-	if flag == 1 {
-		return true
-	}
-	return false
+	return true
 }
