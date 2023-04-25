243ccf88715a9c72e2a553e9e7e5db3aa9f94ad0
Maxime Dubois <maxou.dub76@gmail.com>
2023-04-25 19:52:47 +0200

    1

---
+++ printnbrbase.go
@@ -0,0 +1,43 @@
+package piscine
+
+import (
+	"fmt"
+)
+
+func PrintNbrBase(nbr int, base string) {
+	if len(base) < 2 { //A base must contain at least 2 characters. (condition 1)
+		fmt.Println("NV")
+		return
+	}
+	for i, c := range base { //A base should not contain + or - characters.(condition 3)
+		if c == '-' || c == '+' {
+			fmt.Println("NV")
+			return
+		}
+		for j, s := range base {
+			if i != j && c == s { //Each character of a base must be unique. (condition)
+
+				/*Si i et j sont différents et c et c2 sont identiques,
+				il y a des caractères en double dans "base" donc invalide.*/
+
+				fmt.Println("NV")
+				return
+			}
+		}
+	}
+	var result string
+	negative := nbr < 0
+	if negative {
+		nbr = -nbr //convertit nbr en positif
+	}
+	for nbr > 0 {
+		digit := nbr % len(base)              //correspond au chiffre courant dans la nouvelle base
+		result = string(base[digit]) + result //construis la chaine en ajoutant caracteres par caracteres.56
+		nbr = nbr / len(base)                 //determine le prochain chiffre a convertir.
+	}
+	if negative {
+		result = "-" + result //si negatif affiche le signe "-" devant.
+	}
+
+	fmt.Println(result)
+}
