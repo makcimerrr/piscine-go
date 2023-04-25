68c1954af1d7301dd75df537028ad5287b9cb599
Maxime <maxou.dub76@gmail.com>
2023-04-25 16:54:30 +0200

    1

--- capitalize.go
+++ capitalize.go
@@ -3,7 +3,7 @@ package piscine
 func Capitalize(s string) string {
 	c := []rune(s)
 	for i := 0; i < len(c); i++ {
-		if (c[i] >= 'a' && c[i] <= 'z') && (i == 0 || c[i-1] == ' ' || c[i-1] == '+') {
+		if (c[i] >= 'a' && c[i] <= 'z') && (i == 0 || c[i-1] == ' ' || c[i-1] == '+' || c[i-1] == '\\') {
 			c[i] = c[i] - 32
 		} else if c[i] >= 'A' && c[i] <= 'Z' {
 			c[i] = c[i] + 32
