dfd86663a2fad9eed0e6ec5fe284961bac4ca3b6
Maxime <maxou.dub76@gmail.com>
2023-04-25 13:49:42 +0200

    1

---
+++ isprintable.go
@@ -0,0 +1,10 @@
+package piscine
+
+func IsPrintable(s string) bool {
+	for i := 0; i < len([]rune(s)); i++ {
+		if (rune(s[i]) < 'a' || rune(s[i]) > 'z') && (rune(s[i]) < 'A' || rune(s[i]) > 'Z') && (rune(s[i]) < '0' || rune(s[i]) > '9') {
+			return false
+		}
+	}
+	return true
+}
