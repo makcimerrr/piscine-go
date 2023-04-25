42e73d32e04bd19823256b421dd271fe40334db6
Maxime <maxou.dub76@gmail.com>
2023-04-25 14:03:45 +0200

    1

---
+++ toupper.go
@@ -0,0 +1,16 @@
+package piscine
+
+import (
+	"fmt"
+)
+
+func ToUpper(s string) string {
+	c := []rune(s)
+	for i := 0; i < len([]rune(s)); i++ {
+		if c[i] >= 'a' && c[i] <= 'z' {
+			c[i] = c[i] - 32
+		}
+	}
+	return string(c)
+}
+s
