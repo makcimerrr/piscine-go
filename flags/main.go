9a9b16c810c0ccc43c507acedc144e6b94d9053f
Maxime Dubois <maxou.dub76@gmail.com>
2023-04-26 21:20:42 +0200

    1

--- flags/main.go
+++ flags/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func fHelp() {
-	fmt.Printf("--insert\n  -i\n\t This upper inserts the string into the string passed as argument.\n--order\n  -o\n\t This upper will behave like a boolean, if it is called it will order the argument.\n")
+	fmt.Printf("--insert\n  -i\n\t This flag inserts the string into the string passed as argument.\n--order\n  -o\n\t This flag will behave like a boolean, if it is called it will order the argument.\n")
 }
 
 func TRIEUR(table []rune) string {
