3ab145c131a8d85f503f8083e345e279f9164dc8
Maxime <maxou.dub76@gmail.com>
2023-04-26 10:34:04 +0200

    1

---
+++ printprogramname/main.go
@@ -0,0 +1,26 @@
+package piscine
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	var filename string
+
+	str, _ := os.Executable()
+	lenght := len(str)
+
+	for i := lenght - 1; i >= 0; i-- {
+		if str[i] == '/' {
+			filename = str[i+1:]
+			break
+		}
+	}
+
+	for _, c := range filename {
+		z01.PrintRune(c)
+	}
+	z01.PrintRune('\n')
+}
