2445b4298fa1fd7f80008a69a02d4294d1d00b1b
Maxime Dubois <maxou.dub76@gmail.com>
2023-04-27 10:35:51 +0200

    1

--- concatparams.go
+++ concatparams.go
@@ -1,6 +1,10 @@
 package piscine
 
 func ConcatParams(args []string) string {
+	if len(args) == 0 {
+		return ""
+	}
+
 	result := make([]byte, 0, len(args)*20)
 	for _, arg := range args {
 		result = append(result, []byte(arg)...)
