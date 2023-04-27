76639a0757ca92caf8bea9cfd798ff1067b559ba
Maxime Dubois <maxou.dub76@gmail.com>
2023-04-27 11:57:58 +0200

    1

---
+++ split.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Split(s, sep string) []string {
+
+	var decodage []string
+	chaine := len(s)
+	separateur := len(sep)
+	i := 0
+	for j := 0; j < chaine; j++ {
+		if j+separateur > chaine || s[j:j+separateur] != sep {
+			continue
+		}
+
+		decodage = append(decodage, s[i:j])
+
+		i = j + separateur
+	}
+
+	decodage = append(decodage, s[i:])
+
+	return decodage
+}
