6eb9f5e501a23846e4bad5185cffc2143d345024
Maxime Dubois <maxou.dub76@gmail.com>
2023-04-27 11:53:14 +0200

    1

---
+++ split.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func Split(s, sep string) []string {
+
+	var decodage []string
+	chaine := len(s)
+	separateur := len(sep)
+	i := 0
+	for j := 0; j < chaine; j++ {
+		if j+separateur > chaine || s[j:j+separateur] != sep {
+			continue
+		}
+
+		decodage = append(decodage, s[i:j])
+
+		i = j + separateur
+	}
+
+	decodage = append(decodage, s[i:])
+
+	return decodage
+}
+
+func main() {
+	s := "HelloHAhowHAareHAyou?"
+	fmt.Printf("%#v\n", Split(s, "HA"))
+}
