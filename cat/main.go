1e0428ef83a76f2a54ef7275fcc97d775cd2e2a4
Maxime <maxou.dub76@gmail.com>
2023-05-01 20:27:24 +0200

    1

--- cat/main.go
+++ cat/main.go
@@ -2,37 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	if len(os.Args) == 1 {
 		// Pas d'arguments, lire depuis stdin
-		for {
-			var text string
-			_, err := fmt.Scanln(&text)
-			if err != nil {
-				break
-			}
-			fmt.Println(text)
+		bytes, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+			return
 		}
+		fmt.Print(string(bytes))
 	} else {
 		// Lire depuis chaque fichier donné en argument
 		for _, filename := range os.Args[1:] {
-			file, err := os.Open(filename)
+			bytes, err := ioutil.ReadFile(filename)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
 				continue
 			}
-			defer file.Close()
-			var text string
-			for {
-				_, err := fmt.Fscanln(file, &text)
-				if err != nil {
-					break
-				}
-				fmt.Println(text)
-			}
+			fmt.Print(string(bytes))
 		}
 	}
 }
