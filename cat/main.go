abd95db22491ea239eb97ce0901b72e38ca537ab
Maxime <maxou.dub76@gmail.com>
2023-05-01 20:36:40 +0200

    1

--- cat/main.go
+++ cat/main.go
@@ -2,34 +2,32 @@ package main
 
 import (
 	"io"
-	"io/ioutil"
 	"os"
 
 	"github.com/01-edu/z01"
 )
 
-func main() {
-	arg := os.Args[1:]
-	lenarg := len(arg)
-	if lenarg > 0 {
-		for i := 0; i < lenarg; i++ {
-			data, err := ioutil.ReadFile(arg[i])
-			if err != nil {
-				errorp := "ERROR: open " + string(arg[i]) + ": no such file or directory"
-				PrintStr(errorp)
-				z01.PrintRune('\n')
-				os.Exit(1)
-			}
-			PrintStr(string(data))
-		}
-	} else {
-		io.Copy(os.Stdout, os.Stdin)
+func printStr(s string) {
+	for _, i := range s {
+		z01.PrintRune(i)
 	}
 }
 
-func PrintStr(s string) {
-	a := len(s)
-	for i := 0; i < a; i++ {
-		z01.PrintRune(rune(s[i]))
+func main() {
+	file := os.Args[1:]
+	if len(file) > 0 {
+		for i := 0; i < len(file); i++ {
+			content, err := os.ReadFile(file[i])
+			if err != nil {
+				msgerreur := "ERROR: open " + string(file[i]) + ": no such file or directory\n"
+				printStr(msgerreur)
+				os.Exit(1)
+			}
+			printStr(string(content))
+		}
+	} else {
+		if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
+			panic(err)
+		}
 	}
 }
