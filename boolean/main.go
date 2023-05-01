cbacbaee447e052c5541437e2a6898973d561bef
Maxime <maxou.dub76@gmail.com>
2023-05-01 19:52:24 +0200

    1

---
+++ boolean/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	EvenMsg := "I have an even number of arguments"
+	OddMsg := "I have an odd number of arguments"
+	lengthOfArg := len(os.Args)
+	if isEven(lengthOfArg) {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr == 1 {
+		return true
+	} else {
+		return false
+	}
+}
