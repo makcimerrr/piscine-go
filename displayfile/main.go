8a714c0e7fe38089226fdeb44b9bec60e36c073a
Maxime <maxou.dub76@gmail.com>
2023-05-01 20:16:31 +0200

    1

---
+++ displayfile/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("File name missing")
+		return
+	} else if len(args) > 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+
+	filename := args[1]
+	message, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	fmt.Println(string(message))
+}
