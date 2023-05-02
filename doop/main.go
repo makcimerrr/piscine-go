50f3b6cc245b4a8a51e226c2ab9bc19b63125a7e
Maxime <maxou.dub76@gmail.com>
2023-05-02 15:56:16 +0200

    falc

--- doop/main.go
+++ doop/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
@@ -11,15 +9,15 @@ func main() {
 		return
 	}
 
-	arg1, err := strconv.ParseInt(os.Args[1], 10, 64)
-	if err != nil {
+	arg1 := parseInt64(os.Args[1])
+	if arg1 == nil {
 		return
 	}
 
 	arg2 := os.Args[2]
 
-	arg3, err := strconv.ParseInt(os.Args[3], 10, 64)
-	if err != nil {
+	arg3 := parseInt64(os.Args[3])
+	if arg3 == nil {
 		return
 	}
 
@@ -27,23 +25,23 @@ func main() {
 
 	switch arg2 {
 	case "+":
-		result = arg1 + arg3
+		result = *arg1 + *arg3
 	case "-":
-		result = arg1 - arg3
+		result = *arg1 - *arg3
 	case "*":
-		result = arg1 * arg3
+		result = *arg1 * *arg3
 	case "/":
-		if arg3 == 0 {
-			fmt.Println("No division by 0")
+		if *arg3 == 0 {
+			writeString("No division by 0\n")
 			return
 		}
-		result = arg1 / arg3
+		result = *arg1 / *arg3
 	case "%":
-		if arg3 == 0 {
-			fmt.Println("No modulo by 0")
+		if *arg3 == 0 {
+			writeString("No modulo by 0\n")
 			return
 		}
-		result = arg1 % arg3
+		result = *arg1 % *arg3
 	default:
 		return
 	}
@@ -52,5 +50,46 @@ func main() {
 		return
 	}
 
-	fmt.Println(result)
+	writeInt64(result)
+	writeByte('\n')
+}
+
+func parseInt64(s string) *int64 {
+	var x int64
+	var neg bool
+	if s[0] == '-' {
+		neg = true
+		s = s[1:]
+	}
+	for i := 0; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			return nil
+		}
+		x = x*10 + int64(s[i]-'0')
+	}
+	if neg {
+		x = -x
+	}
+	return &x
+}
+
+func writeString(s string) {
+	var buf [len(s)]byte
+	copy(buf[:], s)
+	os.Stdout.Write(buf[:])
+}
+
+func writeByte(b byte) {
+	os.Stdout.Write([]byte{b})
+}
+
+func writeInt64(n int64) {
+	if n < 0 {
+		writeByte('-')
+		n = -n
+	}
+	if n > 9 {
+		writeInt64(n / 10)
+	}
+	writeByte(byte(n%10) + '0')
 }
