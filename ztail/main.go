e54284a9ba36a3eb526ba2fbae726482dfa494d8
Maxime <maxou.dub76@gmail.com>
2023-05-02 15:24:11 +0200

    audit

--- ztail/main.go
+++ ztail/main.go
@@ -7,29 +7,29 @@ import (
 
 func main() {
 	arg := os.Args[1:]
-	lenarg := len(arg)
-	erreur := false
-	sauterr := false
-	saut := true
+	lenght := len(arg)
+	flag := false
+	flag2 := false
+	flag3 := true
 	if len(arg) > 2 {
 		index := BasicAtoi(arg[1])
-		for i := 2; i < lenarg; i++ {
+		for i := 2; i < lenght; i++ {
 			data, err := os.ReadFile(arg[i])
 			if err != nil {
 				fmt.Printf(err.Error())
 				fmt.Printf("\n")
-				erreur = true
-				sauterr = false
+				flag = true
+				flag2 = false
 			} else if index <= len(data)+1 {
-				if sauterr == false && saut == true && i != 2 {
+				if flag2 == false && flag3 == true && i != 2 {
 					fmt.Printf("\n")
 				}
 				fmt.Printf("==> " + string(arg[i]) + " <==")
 				fmt.Printf("\n")
 				fmt.Printf(string(data[len(data)-index:]))
-				saut = true
+				flag3 = true
 			} else if index > len(data) {
-				if sauterr == false && saut == true {
+				if flag2 == false && flag3 == true {
 					fmt.Printf("\n")
 				}
 				fmt.Printf("==> " + string(arg[i]) + " <==")
@@ -40,7 +40,7 @@ func main() {
 	} else {
 		os.Exit(1)
 	}
-	if erreur == true {
+	if flag == true {
 		os.Exit(1)
 	}
 }
