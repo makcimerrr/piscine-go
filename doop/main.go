89f28b478d0ef3548f66c99ab30604e9844dcef7
Maxime <maxou.dub76@gmail.com>
2023-05-02 16:21:11 +0200

    falc

--- doop/main.go
+++ doop/main.go
@@ -52,6 +52,12 @@ func main() {
 	if arg2 == "-" && ((*arg1 > 0 && *arg3 < 0 && result < 0) || (*arg1 < 0 && *arg3 > 0 && result > 0)) {
 		return
 	}
+	if arg2 == "*" && ((*arg1 > 0 && *arg3 > 0 && result > 0) || (*arg1 < 0 && *arg3 < 0 && result > 0)) {
+		return
+	}
+	if arg2 == "/" && ((*arg1 > 0 && *arg3 < 0 && result < 0) || (*arg1 < 0 && *arg3 > 0 && result > 0)) {
+		return
+	}
 
 	writeInt64(result)
 	writeByte('\n')
