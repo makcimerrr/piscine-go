e0fdcce9f57d3b9d7581485f3330c9f4d7454989
Maxime Dubois <maxou.dub76@gmail.com>
2023-05-04 19:32:58 +0200

    12

--- pilot/main.go
+++ pilot/main.go
@@ -2,25 +2,21 @@ package main
 
 import "fmt"
 
-package main
-
-import "fmt"
-
 const AIRCRAFT1 = 1
 
 type Pilot struct {
-    Name     string
-    Life     float64
-    Age      int
-    Aircraft int
+	Name     string
+	Life     float64
+	Age      int
+	Aircraft int
 }
 
 func main() {
-    donnie := Pilot{
-        Name:     "Donnie",
-        Life:     100.0,
-        Age:      24,
-        Aircraft: AIRCRAFT1,
-    }
-    fmt.Println(donnie)
+	donnie := Pilot{
+		Name:     "Donnie",
+		Life:     100.0,
+		Age:      24,
+		Aircraft: AIRCRAFT1,
+	}
+	fmt.Println(donnie)
 }
