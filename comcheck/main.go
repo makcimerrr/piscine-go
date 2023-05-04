80a9644dd5ddf30ff2b64fe2f07b7f838fd39983
Maxime <maxou.dub76@gmail.com>
2023-05-04 15:12:46 +0200

    555

--- comcheck/main.go
+++ comcheck/main.go
@@ -9,7 +9,7 @@ func main() {
 	arg := os.Args[1:]
 	for i := range arg {
 		if arg[i] == "01" || arg[i] == "galaxy" || arg[i] == "galaxy 01" {
-			fmt.Println("Alert !!!")
+			fmt.Println("Alert!!!")
 		} else {
 			return
 		}
