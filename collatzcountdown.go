93dd6370b8ce208a2ff09c358aedbfccd39e038b
Maxime <maxou.dub76@gmail.com>
2023-05-04 13:21:28 +0200

    454

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -3,9 +3,18 @@ package piscine
 func CollatzCountdown(start int) int {
 	if start <= 0 {
 		return -1
-	} else {
-		return 1
 	}
+	step := 0
+	for start != 1 {
+		if start%2 == 0 {
+			start = start / 2
+			step = step + 1
+		} else {
+			start = start*3 + 1
+			step = step + 1
+		}
+	}
+	return step
 }
 
 /*func main() {
