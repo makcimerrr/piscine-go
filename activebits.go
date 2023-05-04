56c064bc206d1e4ba926fb1c509ffc39e35861bb
Maxime Dubois <maxou.dub76@gmail.com>
2023-05-05 01:22:08 +0200

    1312

--- activebits.go
+++ activebits.go
@@ -3,10 +3,10 @@ package piscine
 func ActiveBits(n int) int {
 	count := 0
 	for n != 0 {
-		if n&1 == 1 { //si trouve le bit 1 ajoute 1 au compteur
+		if n&1 == 1 { /*si trouve le bit 1 ajoute 1 au compteur*/
 			count++
 		}
-		n = n >> 1 //decale d'un crant pour lire les 1
+		n = n >> 1 /*decale d'un crant pour lire les 1*/
 	}
 	return count
 }
