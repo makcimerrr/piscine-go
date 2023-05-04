6885e7b02a98175ec7df74e07d865d0984843d58
Maxime <maxou.dub76@gmail.com>
2023-05-04 15:12:12 +0200

    555

---
+++ comcheck/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arg := os.Args[1:]
+	for i := range arg {
+		if arg[i] == "01" || arg[i] == "galaxy" || arg[i] == "galaxy 01" {
+			fmt.Println("Alert !!!")
+		} else {
+			return
+		}
+	}
+}
