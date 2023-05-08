3c4c6b30ae32b7c6a5dd5f23ac796a6da85bf65c
Maxime <maxou.dub76@gmail.com>
2023-05-08 10:52:51 +0200

    size

---
+++ listsize.go
@@ -0,0 +1,30 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListSize(l *List) int {
+	size := 0
+	for n := l.Head; n != nil; n = n.Next {
+		size++
+	}
+	return size
+}
+
+/*func main() {
+	link := &List{}
+
+	ListPushFront(link, "Hello")
+	ListPushFront(link, "2")
+	ListPushFront(link, "you")
+	ListPushFront(link, "man")
+
+	fmt.Println(ListSize(link))
+}*/
