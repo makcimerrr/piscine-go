ab67c98d14818f1b3b523a85f0dae58319942e30
Maxime <maxou.dub76@gmail.com>
2023-05-08 15:21:56 +0200

    find

--- listfind.go
+++ listfind.go
@@ -1,7 +1,7 @@
 package piscine
 
 /*func ListPushBack(l *List, data interface{}) {
-	n := &NodeL{Data: data}
+	n := &itL{Data: data}
 
 	if l.Tail != nil {
 		l.Tail.Next = n
@@ -26,20 +26,27 @@ func main() {
 	fmt.Println(*found)
 }
 
-type NodeL struct {
+type itL struct {
 	Data interface{}
-	Next *NodeL
+	Next *itL
 }
 
 type List struct {
-	Head *NodeL
-	Tail *NodeL
+	Head *itL
+	Tail *itL
 }*/
 
 func CompStr(a, b interface{}) bool {
 	return a == b
 }
 
-func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
-	return &l.Head.Next.Next.Data
+func ListFind(l List, ref interface{}, comp func(a, b interface{}) bool) interface{} {
+	it := l.Head
+	for it != nil {
+		if comp(it.Data, ref) {
+			return &it.Data
+		}
+		it = it.Next
+	}
+	return nil
 }
