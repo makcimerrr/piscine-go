79b06b07bf54e87c10f225d2db2b2eb61966d145
Maxime <maxou.dub76@gmail.com>
2023-05-09 14:31:31 +0200

    item

---
+++ btreesearchitem.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
+	}
+
+	return root
+}
+
+func main() {
+	root := &TreeNode{Data: "4"}
+	BTreeInsertData(root, "1")
+	BTreeInsertData(root, "7")
+	BTreeInsertData(root, "5")
+	selected := BTreeSearchItem(root, "7")
+	fmt.Print("Item selected -> ")
+	if selected != nil {
+		fmt.Println(selected.Data)
+	} else {
+		fmt.Println("nil")
+	}
+
+	fmt.Print("Parent of selected item -> ")
+	if selected.Parent != nil {
+		fmt.Println(selected.Parent.Data)
+	} else {
+		fmt.Println("nil")
+	}
+
+	fmt.Print("Left child of selected item -> ")
+	if selected.Left != nil {
+		fmt.Println(selected.Left.Data)
+	} else {
+		fmt.Println("nil")
+	}
+
+	fmt.Print("Right child of selected item -> ")
+	if selected.Right != nil {
+		fmt.Println(selected.Right.Data)
+	} else {
+		fmt.Println("nil")
+	}
+}
+
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Data == elem {
+		return root
+	}
+	leftResult := BTreeSearchItem(root.Left, elem)
+	if leftResult != nil {
+		return leftResult
+	}
+	rightResult := BTreeSearchItem(root.Right, elem)
+	if rightResult != nil {
+		return rightResult
+	}
+	return nil
+}
