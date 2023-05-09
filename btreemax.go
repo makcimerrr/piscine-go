2a213e26e41d2ec2f3a80b5fbe7f12059021e7e5
Maxime Dubois <maxou.dub76@gmail.com>
2023-05-09 16:52:04 +0200

    max

---
+++ btreemax.go
@@ -0,0 +1,40 @@
+package piscine
+
+/*type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
+	}
+
+	return root
+}
+
+func main() {
+	root := &TreeNode{Data: "4"}
+	BTreeInsertData(root, "1")
+	BTreeInsertData(root, "7")
+	BTreeInsertData(root, "5")
+	max := BTreeMax(root)
+	fmt.Println(max.Data)
+}*/
+
+func BTreeMax(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	for root.Right != nil {
+		root = root.Right
+	}
+	return root
+}
